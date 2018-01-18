ff1daa28c287c62780cee889aafed8af669a5c4c
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-18 04:29:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray() {
-  
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
 }
 
 // Add your changeCompletely() function here:
